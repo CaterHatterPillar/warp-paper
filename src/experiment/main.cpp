cf661d1932a720348d274ba85df8b93e86a8342c
CaterHatterPillar <CaterHatterPillar@gmail.com>
2013-10-02 16:58:08 +0200

    Added precompiled header file stdafx.h. Implemented simple Windows message loop.

--- src/experiment/main.cpp
+++ src/experiment/main.cpp
@@ -1,3 +1,18 @@
-int main( int argc, char *argv[] ) {
-	return 0;
+#include <stdafx.h>
+
+int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow ) {
+	MSG msgWin = { 0 };
+	while( WM_QUIT!=msgWin.message ) {
+		if( PeekMessage( &msgWin, NULL, 0, 0, PM_REMOVE ) ) {
+			TranslateMessage( &msgWin );
+			DispatchMessage( &msgWin );
+		} else {
+			// Do things.
+		}
+	}
+	return (int)msgWin.wParam;
 }
+
+/*int main( int argc, char *argv[] ) {
+	return 0;
+}*/
---
+++ src/experiment/stdafx.h
@@ -0,0 +1,7 @@
+#ifndef DV2549_EXPERIMENT_STDAFX_H
+#define DV2549_EXPERIMENT_STDAFX_H
+
+#include <windows.h>
+#include <D3D11.h>
+
+#endif // DV2549_EXPERIMENT_STDAFX_H
