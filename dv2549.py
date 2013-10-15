ddc2e237b661bbb833aba5faf2849be7328957b8
CaterHatterPillar <CaterHatterPillar@gmail.com>
2013-10-15 22:15:31 +0200

    Some refactoring in script.

--- dv2549.py
+++ dv2549.py
@@ -1,7 +1,53 @@
+# http://bit.ly/18px6NF
+
 import os
 import sys
 import getopt
 import subprocess
+
+class ResExperiment:
+	def __init__( self ):
+		self.equal 		= True
+		self.timeMs 	= 0.0
+		self.precision 	= 0
+		self.rows		= 0
+		self.cols		= 0
+class ResAnalytics:
+	def __init__( self ):
+		self.devMax = 0.0
+		self.devMin = 0.0
+		self.devStd = 0.0
+
+def readResExperiment():
+	res = open( os.path.abspath( "../../resExperiment.res" ) )
+	lines = [ line.strip() for line in res ]
+	linesf = []
+	for line in lines:
+		lineSplit = line.split( "\t", 2 )
+		linesf.append( lineSplit[ 1 ] )
+	re = ResExperiment()
+	re.equal 		= bool( linesf[ 0 ] )
+	re.timeMs 		= float( linesf[ 1 ] )
+	re.precision 	= int( linesf[ 2 ] )
+	re.rows			= int( linesf[ 3 ] )
+	re.cols			= int( linesf[ 4 ] )
+	return re
+
+def readResAnalytics():
+	res = open( os.path.abspath( "../../resAnalytics.res" ) )
+	lines = [ line.strip() for line in res ]
+	linesf = []
+	for line in lines:
+		lineSplit = line.split( "\t", 2 )
+		linesf.append( lineSplit[ 1 ] )
+	re = ResAnalytics()
+	re.devMax = float( linesf[ 0 ] )
+	re.devMin = float( linesf[ 1 ] )
+	re.devStd = float( linesf[ 2 ] )
+	return re
+
+def writeRes( resExperiment, resAnalytics ):
+	print( resExperiment.timeMs )
 
 def run( cmd ):
 	print( "\nRunning " + cmd + "..." )
@@ -12,21 +58,19 @@ def run( cmd ):
 		stderr=subprocess.STDOUT)
 	return p.communicate()
 
-def loadRes():
-	res = open( os.path.abspath( "../../../res/res.res" ) );
-	lines = [ line.strip() for line in res ]
-	res.close()
-	return lines[ 1 ] # http://bit.ly/18px6NF
-
 def experiment( acc, prec, times ):
-	print( "\nRunning default kernel " + str(times) + " times with properties:\n\tAcceleration: " + acc + "\n\tPrecision: " + prec )
+	#print( "\nRunning default kernel " + str(times) + " times with properties:\n\tAcceleration: " + acc + "\n\tPrecision: " + prec )
 	experimentPath = os.path.abspath( "experiment.exe" )
-	experimentArgs = " " + acc + " " + prec
+	experimentArgs = " TILE " + acc + " " + prec
 	results = []
 	for i in range( 0, times ):
 		run( experimentPath + experimentArgs )
-		results.append( float( loadRes() ) )
+		resExperiment = readResExperiment()
+		resAnalytics = readResAnalytics()
+		writeRes( resExperiment, resAnalytics )
 	return results
+
+# These methods are not used.
 def avg( list ):
 	num = 0
 	sum = 0
@@ -42,60 +86,56 @@ def writeList( file, list ):
 		file.write( str( entry ) + "\n" )
 	file.write( "\n" )
 def write( hard, soft, warp ):
-	out = open( os.path.abspath( "experiment.res" ), "w" )
+	out = open( os.path.abspath( "../../../dv2549.res" ), "w" )
 	writeList( out, hard )
 	writeList( out, soft )
 	writeList( out, warp ) 
 	out.close()
 
 print( "---\ndv2549\n---" )
+# Parse arguments using getopt:
+# try:
+# 	opts, args = getopt.getopt( sys.argv[1:], ":h:|:s:|:w:|:if" )
+# except getopt.GetoptError as e:
+# 	print( e )
+# 	sys.exit( 2 )
+
+# tasks		= []
+# taskArgs 	= []
+# precision 	= "INT"
+# # Interpret arguments parsed to script:
+# for opt, arg in opts:
+# 	if opt in ( "-h", "-s", "-w" ): # Task
+# 		if opt=="-h": # Hardware
+# 			tasks.append( "HARD" )
+# 		elif opt=="-s": # Software
+# 			tasks.append( "SOFT" )
+# 		elif opt=="-w": # WARP
+# 			tasks.append( "WARP" )
+# 		taskArgs.append( int( arg ) )
+# 	if opt in ( "-i", "-f" ): # Precision
+# 		if opt == "-i": # Integer
+# 			precision = "INT"
+# 		elif opt == "-f": # Float
+# 			precision = "FLOAT"
+
+precision = "FLOAT"
 
 # Generate a viable matrix:
-matrixgenArgPrecison 	= "INT"
-matrixgenArgDimension 	= "200"
-matrixgenArgMin 		= "0"
-matrixgenArgMax 		= "10"
+matrixgenArgPrecison 	= precision
+matrixgenArgDimension 	= "200"	
+matrixgenArgMin 		= "0"	# Currently default values.
+matrixgenArgMax 		= "10"	
 matrixgenArgs 			= " " + matrixgenArgPrecison + " " + matrixgenArgDimension + " " + matrixgenArgMin + " " + matrixgenArgMax
-matrixgenPath 			= os.path.abspath( "../../matrixgen/Release/matrixgen.exe" )
+matrixgenPath 			= os.path.abspath( "../../Release/matrixgen.exe" )
 matrixgenCommand 		= matrixgenPath + matrixgenArgs
 print( "\nGenerating matrices with properties:\n\tPrecision: " + matrixgenArgPrecison + "\n\tDimension: " + matrixgenArgDimension + "\n\tMax: " + matrixgenArgMax + "\n\tMin: " + matrixgenArgMin )
 run( matrixgenCommand )
 
-# Execute tests:
-experimentArgPrecision = "INT"
-resultsHard = experiment( "HARD", experimentArgPrecision, 100 )
-resultsSoft = experiment( "SOFT", experimentArgPrecision, 100 )
-resultsWarp = experiment( "WARP", experimentArgPrecision, 100 )
-write( resultsHard, resultsSoft, resultsWarp )
+# Perform experiments:
+#for i, task in enumerate( tasks ):
+#	experiment( task, precision, taskArgs[ i ] )
+
+experiment( "HARD", precision, 10 )
 
 input( "Finished execution." )
-
-# Parse arguments using getopt:
-# try:
-# 	opts, args = getopt.getopt( sys.argv[1:], ":n:|:h:d:|:c2rFtf" )
-# except getopt.GetoptError as e:
-# 	print( e )
-# 	sys.exit( 2 )
-	
-# Interpret arguments parsed to script:
-#for opt, arg in opts:
-#if opt == "-n":
-#printLimit = int( arg )
-#if opt in ( "-h", "-d" ):
-#if opt == "-h": # Now - Hours
-#date = date + datetime.timedelta( hours = -int(arg) )
-#elif opt == "-d": # Now - Days
-#date = date + datetime.timedelta( days = -int(arg) )
-#if opt in ( "-c", "-2", "-r", "-F", "-t", "-f" ):
-#if opt == "-c":
-#q = Queries.Queries_c
-#elif opt == "-2":
-#q = Queries.Queries_2
-#elif opt == "-r":
-#q = Queries.Queries_r
-#elif opt == "-F":
-#q = Queries.Queries_F
-#elif opt == "-t":
-#q = Queries.Queries_t
-#elif opt == "-f":
-#q = Queries.Queries_f
