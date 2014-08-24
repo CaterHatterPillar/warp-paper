8dd2b8d20845dc38610628094e8d5a30dd62c4bf
Eric Nilsson <CaterHatterPillar@gmail.com>
2014-08-24 11:06:08 +0200

    Added presentation build using the bth-beamer-style.

--- build.pl
+++ build.pl
@@ -1,5 +1,7 @@
 #!/usr/local/bin/perl
 
+use File::Copy;
+
 sub help_and_exit{
     print "Usage: build.pl paper|presentation\n";
     exit;
@@ -19,9 +21,12 @@ if($target eq "paper"){
     system("bibtex GAME_HARD_01.aux");
     system("pdflatex -shell-escape GAME_HARD_01.tex");
     system("pdflatex -shell-escape GAME_HARD_01.tex");
+    copy("GAME_HARD_01.pdf", "../GAME_HARD_01.pdf") or die "Failed to copy: $!";
 } elsif($target eq "presentation"){
-    print "Not yet implemented!";
-    exit;
+    chdir "presentation/";
+    system("pdflatex GAME_HARD_01_PRESENTATION.tex");
+    copy("GAME_HARD_01_PRESENTATION.pdf", "../GAME_HARD_01_PRESENTATION.pdf")
+        or die "Failed to copy: $!";
 } else{
     help_and_exit();
 }
---
+++ presentation/README.md
@@ -0,0 +1,4 @@
+bth-beamer-style
+================
+
+Blekinge Institute of Technology LaTeX Beamer presentation style. Licensed under the BSD Clause-3 license.
